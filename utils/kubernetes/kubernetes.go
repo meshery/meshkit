c7f98c8ff67b789e4e74ddbb167b2f19c2e6c089
kumarabd <abishekkumar92@gmail.com>
2021-02-25 02:31:11 +0530

    removed residues
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- utils/kubernetes/kubernetes.go
+++ utils/kubernetes/kubernetes.go
@@ -1,9 +1,6 @@
 package kubernetes
 
 import (
-	"github.com/layer5io/meshkit/utils/kubernetes/discovery"
-	"github.com/layer5io/meshkit/utils/kubernetes/informer"
-
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -11,8 +8,6 @@ import (
 
 type Client struct {
 	RestConfig        rest.Config           `json:"restconfig,omitempty"`
-	DiscoveryClient   *discovery.Client     `json:"discovery_client,omitempty"`
-	InformerClient    *informer.Client      `json:"informer_client,omitempty"`
 	KubeClient        *kubernetes.Clientset `json:"kubeclient,omitempty"`
 	DynamicKubeClient dynamic.Interface     `json:"dynamic_kubeclient,omitempty"`
 }
@@ -36,23 +31,9 @@ func New(kubeconfig []byte) (*Client, error) {
 		return nil, ErrNewDynClient(err)
 	}
 
-	// Configure discovery client
-	dclient, err := discovery.NewClient(kclient)
-	if err != nil {
-		return nil, ErrNewDiscovery(err)
-	}
-
-	// Configure informer client
-	iclient, err := informer.NewClient(kclient)
-	if err != nil {
-		return nil, ErrNewInformer(err)
-	}
-
 	return &Client{
 		RestConfig:        *restConfig,
 		DynamicKubeClient: dyclient,
-		DiscoveryClient:   dclient,
-		InformerClient:    iclient,
 		KubeClient:        kclient,
 	}, nil
 }
