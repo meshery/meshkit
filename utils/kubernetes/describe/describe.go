acd5810842472d2c69ab9b1490067f7e62feeef0
Piyush Singariya <piyushsingariya@gmail.com>
2022-03-05 16:00:35 +0530

    going to the moon
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- utils/kubernetes/describe/describe.go
+++ utils/kubernetes/describe/describe.go
@@ -1,9 +1,15 @@
 package describe
 
 import (
-	"fmt"
-
-	"k8s.io/client-go/kubernetes"
+	meshkitkube "github.com/layer5io/meshkit/utils/kubernetes"
+	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	certificatesv1beta1 "k8s.io/api/certificates/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	discoveryv1 "k8s.io/api/discovery/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/kubectl/pkg/describe"
 )
 
@@ -25,31 +31,78 @@ const (
 	Pod
 	Namespace
 	Job
+	CronJob
+	Deployment
+	DaemonSet
+	ReplicaSet
+	StatefulSet
+	Secret
+	ServiceAccount
+	Node
+	LimitRange
+	ResourceQuota
+	PersistentVolume
+	PersistentVolumeClaim
+	Endpoints
+	ConfigMap
+	PriorityClass
+	Ingress
+	Role
+	ClusterRole
+	RoleBinding
+	ClusterRoleBinding
+	NetworkPolicy
+	ReplicationController
+	CertificateSigningRequest
+	EndpointSlice
 )
 
-func Describe(client kubernetes.Interface, options DescriberOptions) (string, error) {
-	var settings describe.DescriberSettings
-	settings.ShowEvents = options.ShowEvents
-	settings.ChunkSize = options.ChunkSize
+var ResourceMap = map[DescribeType]schema.GroupKind{
+	Pod:                       {Group: corev1.GroupName, Kind: "Pod"},
+	Deployment:                {Group: appsv1.GroupName, Kind: "Deployment"},
+	Job:                       {Group: batchv1.GroupName, Kind: "Job"},
+	CronJob:                   {Group: batchv1.GroupName, Kind: "CronJob"},
+	StatefulSet:               {Group: appsv1.GroupName, Kind: "StatefulSet"},
+	DaemonSet:                 {Group: appsv1.GroupName, Kind: "DaemonSet"},
+	ReplicaSet:                {Group: appsv1.GroupName, Kind: "ReplicaSet"},
+	Secret:                    {Group: corev1.GroupName, Kind: "Secret"},
+	Service:                   {Group: corev1.GroupName, Kind: "Service"},
+	ServiceAccount:            {Group: corev1.GroupName, Kind: "ServiceAccount"},
+	Node:                      {Group: corev1.GroupName, Kind: "Node"},
+	LimitRange:                {Group: corev1.GroupName, Kind: "LimitRange"},
+	ResourceQuota:             {Group: corev1.GroupName, Kind: "ResourceQuota"},
+	PersistentVolume:          {Group: corev1.GroupName, Kind: "PersistentVolume"},
+	PersistentVolumeClaim:     {Group: corev1.GroupName, Kind: "PersistentVolumeClaim"},
+	Namespace:                 {Group: corev1.GroupName, Kind: "Namespace"},
+	Endpoints:                 {Group: corev1.GroupName, Kind: "Endpoints"},
+	ConfigMap:                 {Group: corev1.GroupName, Kind: "ConfigMap"},
+	PriorityClass:             {Group: corev1.GroupName, Kind: "PriorityClass"},
+	Ingress:                   {Group: networkingv1.GroupName, Kind: "Ingress"},
+	Role:                      {Group: rbacv1.GroupName, Kind: "Role"},
+	ClusterRole:               {Group: rbacv1.GroupName, Kind: "ClusterRole"},
+	RoleBinding:               {Group: rbacv1.GroupName, Kind: "RoleBinding"},
+	ClusterRoleBinding:        {Group: rbacv1.GroupName, Kind: "ClusterRoleBinding"},
+	NetworkPolicy:             {Group: networkingv1.GroupName, Kind: "NetworkPolicy"},
+	ReplicationController:     {Group: corev1.GroupName, Kind: "ReplicationController"},
+	CertificateSigningRequest: {Group: certificatesv1beta1.GroupName, Kind: "CertificateSigningRequest"},
+	EndpointSlice:             {Group: discoveryv1.GroupName, Kind: "EndpointSlice"},
+}
 
-	switch options.Type {
-	case Pod:
-		var describer *describe.PodDescriber
-		describer.Interface = client
-		return describer.Describe(options.Namespace, options.Name, settings)
-	case Namespace:
-		var describer *describe.NamespaceDescriber
-		describer.Interface = client
-		return describer.Describe(options.Namespace, options.Name, settings)
-	case Service:
-		var describer *describe.ServiceDescriber
-		describer.Interface = client
-		return describer.Describe(options.Namespace, options.Name, settings)
-	case Job:
-		var describer *describe.JobDescriber
-		describer.Interface = client
-		return describer.Describe(options.Namespace, options.Name, settings)
-	default:
-		return "", fmt.Errorf("invalid kubernetes object type or object type not supported in meshkit")
+func Describe(client *meshkitkube.Client, options DescriberOptions) (string, error) {
+	// getting schema.GroupKind from Resource map
+	kind := ResourceMap[options.Type]
+	describer, ok := describe.DescriberFor(kind, &client.RestConfig)
+	if !ok {
+		return "", ErrGetDescriberFunc()
 	}
+
+	describerSetting := describe.DescriberSettings{
+		ShowEvents: options.ShowEvents,
+	}
+	output, err := describer.Describe(options.Namespace, options.Name, describerSetting)
+	if err != nil {
+		return "", err
+	}
+
+	return output, nil
 }
---
+++ utils/kubernetes/describe/error.go
@@ -0,0 +1,17 @@
+package describe
+
+import "github.com/layer5io/meshkit/errors"
+
+var (
+	ErrGetDescriberFuncCode = "not set"
+)
+
+func ErrGetDescriberFunc() error {
+	return errors.New(
+		ErrGetDescriberFuncCode,
+		errors.Fatal,
+		[]string{"Failed to get describer for the resource"},
+		[]string{"invalid kubernetes object type or object type not supported in meshkit", "Describer not found for the defined Resource"},
+		nil, nil,
+	)
+}
