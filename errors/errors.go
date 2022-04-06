15b5ee7958ab144fff11fd5a8194247815357ff3
leecalcote <leecalcote@gmail.com>
2022-04-06 14:01:43 -0500

    merge master; add makefiles
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- errors/errors.go
+++ errors/errors.go
@@ -39,7 +39,6 @@ import "strings"
 
 // Deprecated: NewDefault is deprecated, use New(...) instead.
 func NewDefault(code string, ldescription ...string) *Error {
-
 	return &Error{
 		Code:                 code,
 		Severity:             None,
@@ -72,7 +71,6 @@ func NewDefault(code string, ldescription ...string) *Error {
 //	            []string{"Endpoint might not be reachable"},
 //	            []string{"Make sure the NATS endpoint is reachable"})
 func New(code string, severity Severity, sdescription []string, ldescription []string, probablecause []string, remedy []string) *Error {
-
 	return &Error{
 		Code:                 code,
 		Severity:             severity,
@@ -86,7 +84,6 @@ func New(code string, severity Severity, sdescription []string, ldescription []s
 func (e *Error) Error() string { return strings.Join(e.LongDescription[:], ".") }
 
 func GetCode(err error) string {
-
 	if obj := err.(*Error); obj != nil && obj.Code != " " {
 		return obj.Code
 	}
@@ -94,7 +91,6 @@ func GetCode(err error) string {
 }
 
 func GetSeverity(err error) Severity {
-
 	if obj := err.(*Error); obj != nil {
 		return obj.Severity
 	}
@@ -102,7 +98,6 @@ func GetSeverity(err error) Severity {
 }
 
 func GetSDescription(err error) string {
-
 	if obj := err.(*Error); obj != nil {
 		return strings.Join(err.(*Error).ShortDescription[:], ".")
 	}
@@ -110,7 +105,6 @@ func GetSDescription(err error) string {
 }
 
 func GetCause(err error) string {
-
 	if obj := err.(*Error); obj != nil {
 		return strings.Join(err.(*Error).ProbableCause[:], ".")
 	}
@@ -118,7 +112,6 @@ func GetCause(err error) string {
 }
 
 func GetRemedy(err error) string {
-
 	if obj := err.(*Error); obj != nil {
 		return strings.Join(err.(*Error).SuggestedRemediation[:], ".")
 	}
