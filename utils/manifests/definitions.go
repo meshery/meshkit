86d7967618efff715ca5f06b4da41face8ae8b15
Nithish <nithishkarthik@Nithishs-MacBook-Pro.local>
2022-05-27 16:34:32 +0530

    docs: Add comments
    Signed-off-by: Nithish <nithishkarthik@Nithishs-MacBook-Pro.local>

--- utils/manifests/definitions.go
+++ utils/manifests/definitions.go
@@ -14,74 +14,32 @@ const (
 	MESHERY
 )
 
-// Json Paths
-type JsonPath []string
 type Component struct {
 	Schemas     []string
 	Definitions []string
 }
 
 // all the data that is needed to get a certain value should be present in the config created by the adapter.
-
 type Config struct {
-	Name            string                         // Name of the service mesh,or k8 or meshery
-	Type            string                         //Type of the workload like- Istio, TraefikMesh, Kuma, OSM, Linkerd,AppMesh,NginxMesh
-	MeshVersion     string                         // For service meshes
-	K8sVersion      string                         //For K8ss
-	ModifyDefSchema func(*string, *string)         //takes in definition and schema, does some manipulation on them and returns the new def and schema
-	CrdFilter       CueCrdFilter                   // these filters are a representation of the things that are needed for generating a component from a manifest
-	ExtractCrds     func(manifest string) []string // takes in the manifest and returns a list of crds
-}
-
-/* How to customize these filters (These comments to be updated if the behavior changes in future)-
-There are only two types of filters used internally by kubeopenapi-jsonschema which is being used here- filter(yinput filter) and output filter.
-The filters described below are an abstraction over those filters.
-1. RootFilter- is used at two places
-	(a) For fetching the crd names or api resources on which we will iterate over, first we apply the root filter to get the objects we are interested in and then
-		the NameFilter is applied as output filter to take out just the names from selected objects.
-	(b) [this will be discussed after ItrFilter]
-
-2. NameFilter- As explained above, it is used as an --o-filter to extract only the names after RootFilter(1(a)) has been applied.
-3. ItrFilter- The first element of the array is an incomplete filter and expected to end with "=='crdname')]". So it needs to be configured just before the double
-				equals sign. The rest of the elements of the array are directly appended afterwards to form the complete filter.
-
-4. GroupFilter- After ItrFilter gives us the object with the group and version of the crd/resource we are interested in with this iteration, GroupFilter is used as output filter to only extract the object with group in one of the fields.
-5. VersionFilter- After ItrFilter gives us the object with the group and version of the crd/resource we are interested in with this iteration, GroupFilter is used as output filter to only extract the object with version in one of the fields.
-6. ItrSpecFilter- Functionally is same as ItrFilter but instead of group and version, it is used to get openapi spec/schema.
-7. GField- The GroupFilter returns a json which has group in it. The key name which is used to signify group will be passed here. For eg: "group"
-8. VField- The VersionFilter returns a json which has version in it. The key name which is used to signify version will be passed here. For eg: "version", "name","version-name"
-9. OnlyRes- In some cases we dont want to compute crdnames/api-resources at runtime as we already have them. Pass those names as an array here to skip that step.
-10. IsJson- The file on which to apply all these filters is by default expected to be YAML. Set this to true if a JSON is passed instead. (These are the only two supported formats)
-11. SpecFilter- When SpecFilter is passed, it is applied as output filter after ItrSpec filter.
-12. ResolveFilter- If passed, this filter will be applied after all the filter for generating schema. This filter is used to resolve $ref in json schema
-1(b) If SpecFilter is not passed, then before the ItrSpecFilter the rootfilter will be applied by default and then the ItrSpec filter will be applied as output filter.
-
-*/
-type CrdFilter struct {
-	RootFilter    JsonPath
-	NameFilter    JsonPath
-	GroupFilter   JsonPath
-	VersionFilter JsonPath
-	SpecFilter    JsonPath
-	ItrFilter     JsonPath
-	ItrSpecFilter JsonPath
-	ResolveFilter JsonPath
-	VField        string
-	GField        string
-	IsJson        bool
-	OnlyRes       []string
+	Name            string                 // Name of the service mesh,or k8 or meshery
+	Type            string                 //Type of the workload like- Istio, TraefikMesh, Kuma, OSM, Linkerd,AppMesh,NginxMesh
+	MeshVersion     string                 // For service meshes
+	K8sVersion      string                 //For K8ss
+	ModifyDefSchema func(*string, *string) //takes in definition and schema, does some manipulation on them and returns the new def and schema
+	CrdFilter       CueCrdFilter
+	ExtractCrds     func(manifest string) []string //takes in the manifest and returns a list of all the crds
 }
 
 // takes in the parsed root cue value of the CRD as its input and returns the extracted value
 type CueFilter func(rootCRDCueVal cue.Value) (cue.Value, error)
 
-// these are basically getter functions
-// these are filters for a single CRD
-// these filters are a reflection of the things that are needed for generating a Component
+// basically getter functions
+// applicable for a single CRD
+// can be intrepreted as the things that are needed for generating a Component
 type CueCrdFilter struct {
 	NameExtractor       CueFilter
 	GroupExtractor      CueFilter
 	VersionExtractor    CueFilter
 	SpecExtractor       CueFilter
-	IdentifierExtractor CueFilter // identifiers are the values that uniquely identify a CRD
+	IdentifierExtractor CueFilter // identifiers are the values that uniquely identify a CRD (in most of the cases, it is the 'Name' field)
 }
--- utils/manifests/generateComponent.go
+++ utils/manifests/generateComponent.go
@@ -9,32 +9,30 @@ import (
 )
 
 func GenerateComponents(ctx context.Context, manifest string, resource int, cfg Config) (*Component, error) {
-
 	var crds []string
 	var c = &Component{
 		Schemas:     []string{},
 		Definitions: []string{},
 	}
-
 	crds = cfg.ExtractCrds(manifest)
-
 	for _, crd := range crds {
-
-		file, err := yaml.Extract("dummy", crd)
+		file, err := yaml.Extract("crds", crd) // first arguement is dummy
 		if err != nil {
 			return nil, err
 		}
-
 		cueCtx := cuecontext.New()
 		parsedCrd := cueCtx.BuildFile(file)
-
 		outDef, err := getDefinitions(parsedCrd, resource, cfg, ctx)
 		if err != nil {
+			// inability to generate component for a single crd should not affect the rest
+			// TODO: Maintain a list of errors and keep pushing the errors to the list so that it can be displayed at last
 			continue
 			// return nil, err
 		}
 		outSchema, err := getSchema(parsedCrd, cfg, ctx)
 		if err != nil {
+			// inability to generate component for a single crd should not affect the rest
+			// TODO: Maintain a list of errors and keep pushing the errors to the list so that it can be displayed at last
 			continue
 			// return nil, ErrGetSchemas(err)
 		}
@@ -44,7 +42,6 @@ func GenerateComponents(ctx context.Context, manifest string, resource int, cfg 
 		c.Definitions = append(c.Definitions, outDef)
 		c.Schemas = append(c.Schemas, outSchema)
 	}
-
 	fmt.Printf("%v", c)
 	return c, nil
 }
