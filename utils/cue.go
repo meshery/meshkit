2cb11177b6ebf7ecfd913c304e22a8b2ceb97e66
Nithish <nithishkarthik01@gmail.com>
2022-08-08 16:53:23 +0530

    feat: add cue util to convert jsonschema to cue
    Signed-off-by: Nithish <nithishkarthik01@gmail.com>

--- utils/cue.go
+++ utils/cue.go
@@ -3,7 +3,9 @@ package utils
 import (
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/format"
 	"cuelang.org/go/encoding/json"
+	"cuelang.org/go/encoding/jsonschema"
 	"cuelang.org/go/encoding/yaml"
 )
 
@@ -39,6 +41,34 @@ func YamlToCue(value string) (cue.Value, error) {
 	out = cuectx.BuildFile(expr)
 	if out.Err() != nil {
 		return out, ErrYamlToCue(out.Err())
+	}
+	return out, nil
+}
+
+func JsonSchemaToCue(value string) (cue.Value, error) {
+	var out cue.Value
+	jsonSchema, err := json.Extract("", []byte(value))
+	if err != nil {
+		return out, ErrJsonSchemaToCue(err)
+	}
+	cueCtx := cuecontext.New()
+	cueJsonSchemaExpr := cueCtx.BuildExpr(jsonSchema)
+	if err := cueJsonSchemaExpr.Err(); err != nil {
+		return out, ErrJsonSchemaToCue(err)
+	}
+	extractedSchema, err := jsonschema.Extract(cueJsonSchemaExpr, &jsonschema.Config{
+		PkgName: "jsonschemeconv",
+	})
+	if err != nil {
+		return out, ErrJsonSchemaToCue(err)
+	}
+	src, err := format.Node(extractedSchema)
+	if err != nil {
+		return out, ErrJsonSchemaToCue(err)
+	}
+	out = cueCtx.CompileString(string(src))
+	if out.Err() != nil {
+		return out, ErrJsonSchemaToCue(out.Err())
 	}
 	return out, nil
 }
