23b35798f7a60241f7e8f75aaa24d8d6301fa164
ashish <ashishjaitiwari15112000@gmail.com>
2022-08-16 01:16:10 +0530

    Add go.mod sync utility
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

---
+++ cmd/syncmodutil/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/layer5io/meshkit/cmd/syncmodutil/internal/modsync"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatal("modsync <src> <dest>")
+	}
+	src := os.Args[1]
+	dest := os.Args[2]
+	f, err := os.Open(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	g, err := modsync.New(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f2, err := os.Open(dest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	newgomod, err := g.SyncRequire(f2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile(dest, []byte(newgomod), 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.PrintReplacedVersions()
+}
