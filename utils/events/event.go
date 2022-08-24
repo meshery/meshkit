c96be6cdad6773125fbf296b80adfe8959f684b0
ashish <ashishjaitiwari15112000@gmail.com>
2022-08-25 00:38:12 +0530

    Remove buffer from Event struct
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- utils/events/event.go
+++ utils/events/event.go
@@ -4,50 +4,29 @@ import (
 	"sync"
 )
 
-type EventBuffer struct {
-	circularqueue  []interface{}
+type EventStreamer struct {
 	clientChannels []chan interface{}
-	last           int //last=-1 means queue is empty
-	mx             sync.Mutex
 	clmx           sync.Mutex
 }
 
-func NewEventBuffer(size int) *EventBuffer {
-	return &EventBuffer{
-		circularqueue: make([]interface{}, size),
-		last:          0,
+func NewEventStreamer() *EventStreamer {
+	return &EventStreamer{
+		clientChannels: make([]chan interface{}, 0),
 	}
 }
 
-// Each event will be first stored in a circular queue to return the last n operations whenever a new client connects.
-// After that, further events will be pushed to the client.
-func (e *EventBuffer) Enqueue(i interface{}) {
-	e.mx.Lock()
-	defer e.mx.Unlock()
-	pos := e.last % len(e.circularqueue)
-	e.circularqueue[pos] = i
-	e.last++
-	go func() {
-		for _, ch := range e.clientChannels {
+func (e *EventStreamer) Publish(i interface{}) {
+	e.clmx.Lock()
+	defer e.clmx.Unlock()
+	for _, ch := range e.clientChannels {
+		go func(ch chan interface{}) {
 			ch <- i
-		}
-	}()
-}
-func (e *EventBuffer) Copy(client chan interface{}) {
-	e.mx.Lock()
-	defer e.mx.Unlock()
-	var events []interface{}
-	for i := 0; i < e.last%len(e.circularqueue); i++ {
-		ev := e.circularqueue[i]
-		events = append(events, ev)
-		go func(ev interface{}) {
-			client <- ev
-		}(ev)
+		}(ch)
 	}
-	return
+
 }
 
-func (e *EventBuffer) Subscribe(ch chan interface{}) {
+func (e *EventStreamer) Subscribe(ch chan interface{}) {
 	e.clmx.Lock()
 	defer e.clmx.Unlock()
 	e.clientChannels = append(e.clientChannels, ch)
