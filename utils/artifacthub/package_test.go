2d3d212c71918dca2ab59e5b5234a9f522f53b0f
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2022-09-14 21:24:38 +0530

    Fix lint checks
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- utils/artifacthub/package_test.go
+++ utils/artifacthub/package_test.go
@@ -9,7 +9,7 @@ func TestGetChartUrl(t *testing.T) {
 	}{
 		// these might change in the future, so the tests have to be changed as well when the urls change
 		// because the urls will change with every new version update to the package
-		{AhPackage{Name: "consul", Repository: "bitnami", Organization: ""}, "https://charts.bitnami.com/bitnami/consul-10.8.1.tgz"},
+		{AhPackage{Name: "consul", Repository: "bitnami", Organization: ""}, "https://charts.bitnami.com/bitnami/consul-10.8.3.tgz"},
 		{AhPackage{Name: "crossplane-types", Repository: "crossplane", Organization: ""}, "https://charts.crossplane.io/master/crossplane-types-0.13.0-rc.98.g1eb0776.tgz"},
 	}
 	for _, tt := range tests {
