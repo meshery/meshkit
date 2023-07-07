b10c06acd7955c92ae1e4f02b409f251dd4c7b4f
MUzairS15 <muzair.shaikh810@gmail.com>
2023-07-07 19:41:36 +0530

    lint fixes
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- utils/component/error.go
+++ utils/component/error.go
@@ -20,9 +20,9 @@ func ErrGetDefinition(err error) error {
 }
 
 func ErrGetSchema(err error) error {
-	return errors.New(ErrGetSchemaCode, errors.Alert, []string{"Could not get schema for the given CRD"}, []string{err.Error()}, []string{"Unable to marshal from cue value to valid JSON", "Unable to unmarshal from JSON to Go type"}, []string{"Verify CRD has valid schema.", "Malformed JSON", "CUE path to propery doesn't exist"})
+	return errors.New(ErrGetSchemaCode, errors.Alert, []string{"Could not get schema for the given CRD"}, []string{err.Error()}, []string{"Unable to marshal from cue value to JSON", "Unable to unmarshal from JSON to Go type"}, []string{"Verify CRD has valid schema.", "Malformed JSON provided", "CUE path to propery doesn't exist"})
 }
 
 func ErrUpdateSchema(err error, obj string) error {
-	return errors.New(ErrUpdateSchemaCode, errors.Alert, []string{"Failed to update schema properties for ", obj}, []string{err.Error()}, []string{"Incorrect type assertion", "Selector.Unquoted might have been invoked on non-string label", "error during conversion from cue.Selector to string"}, []string{"Ensure correct type assertion", "Perform appropriate conversion from cue.Selector to string","Verify CRD has valid schema"})
+	return errors.New(ErrUpdateSchemaCode, errors.Alert, []string{"Failed to update schema properties for ", obj}, []string{err.Error()}, []string{"Incorrect type assertion", "Selector.Unquoted might have been invoked on non-string label", "error during conversion from cue.Selector to string"}, []string{"Ensure correct type assertion", "Perform appropriate conversion from cue.Selector to string", "Verify CRD has valid schema"})
 }
