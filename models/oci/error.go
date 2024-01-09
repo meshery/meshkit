0adfd7e8cce110dabedeee953653468b531e2013
Pranav Singh <pranavsingh02@hotmail.com>
2024-01-09 18:53:55 +0530

    add meshkit errs
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- models/oci/error.go
+++ models/oci/error.go
@@ -10,6 +10,9 @@ var (
 	ErrAppendingLayerCode = "11107"
 	ErrReadingFileCode         = "11108"
 	ErrUnSupportedLayerTypeCode = "11109"
+	ErrGettingLayerCode = "11110"
+	ErrCompressingLayerCode = "11111"
+	ErrUnTaringLayerCode = "11112"
 )
 
 func ErrAppendingLayer(err error) error {
@@ -23,5 +26,20 @@ func ErrReadingFile(err error) error {
 func ErrUnSupportedLayerType(err error) error {
 	return errors.New(ErrUnSupportedLayerTypeCode, errors.Alert, []string{"unsupported layer type"}, []string{err.Error()}, []string{"layer type is not supported"}, []string{"Try using a different layer type", fmt.Sprintf("supported layer types are: %s, %s", LayerTypeTarball, LayerTypeStatic)})
 }
+
+func ErrGettingLayer(err error) error {
+	return errors.New(ErrGettingLayerCode, errors.Alert, []string{"getting layer failed"}, []string{err.Error()}, []string{"failed to get the layer"}, []string{"Try using a different layer", "check if OCI image is not malformed"})
+}
+
+func ErrCompressingLayer(err error) error {
+	return errors.New(ErrCompressingLayerCode, errors.Alert, []string{"compressing layer failed"}, []string{err.Error()}, []string{"failed to compress the layer"}, []string{"Try using a different layer", "check if layers are compatible with the base image"})
+}
+
+func ErrUnTaringLayer(err error) error {
+	return errors.New(ErrUnTaringLayerCode, errors.Alert, []string{"untaring layer failed"}, []string{err.Error()}, []string{"failed to untar the layer"}, []string{"Try using a different layer", "check if image is not malformed"})
+}
+
+
+
 
 
