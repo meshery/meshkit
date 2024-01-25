0a044c4682c46c314971e082ecb1631f26506d45
MUzairS15 <muzair.shaikh810@gmail.com>
2024-01-26 00:23:15 +0530

    update interceptors:
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- generators/github/git_repo.go
+++ generators/github/git_repo.go
@@ -31,12 +31,16 @@ func (gr GitRepo) GetContent() (models.Package, error) {
 		return nil, err
 	}
 
-	filePath := fmt.Sprintf("%s_%s_%s_%s", owner, repo, branch, utils.GetRandomAlphabetsOfDigit(5))
+	filePath := fmt.Sprintf("%s%s_%s_%s_%s", os.TempDir(), owner, repo, branch, utils.GetRandomAlphabetsOfDigit(5))
 	fd, err := os.Create(filePath) // perform cleanup
 	if err != nil {
 		return nil, utils.ErrCreateFile(err, filePath)
 	}
 	br := bufio.NewWriter(fd)
+
+	defer func() {
+		_ = br.Flush()
+	}()
 	err = gitWalker.
 		Owner(owner).
 		Repo(repo).
@@ -48,11 +52,9 @@ func (gr GitRepo) GetContent() (models.Package, error) {
 		Walk()
 
 	if err != nil {
-		fmt.Println(err, "ERR WALKING ")
 		return nil, walker.ErrCloningRepo(err)
 	}
 
-	fmt.Println("TEST SUCCESUGLY CLONED")
 	return GitHubPackage{
 		Name:       gr.PackageName,
 		filePath:   filePath,
@@ -66,12 +68,20 @@ func (gr GitRepo) GetContent() (models.Package, error) {
 
 func (gr GitRepo) extractRepoDetailsFromSourceURL() (owner, repo, branch, versionTag, root string, err error) {
 	parts := strings.SplitN(strings.TrimPrefix(gr.URL.Path, "/"), "/", 5)
-	if len(parts) == 5 {
+	size := len(parts)
+	if size >= 4 {
 		owner = parts[0]
 		repo = parts[1]
 		branch = parts[2]
-		versionTag = parts[3]
-		root = parts[4]
+		// The version is used to clone a repo at a particular tag, when specifying the URL, the format is <git://github.com/owner/repo/branch/versiontag/root(path to the directory/file)>, the optional version attribute if provided is at 3rd index.
+		// And hence the part after the 
+		if size > 4 {
+			versionTag = parts[3]
+			root = parts[4]
+			} else {
+			// If the url doesn't contain the version, meaning we will include all tags and all the remaining part after branch/ will be considered as location inside the repo.
+			root = parts[3]
+		}
 	} else {
 		err = ErrInvalidGitHubSourceURL(fmt.Errorf("specify owner, repo, branch and filepath in the url according to the specified source url format"))
 	}
@@ -80,11 +90,8 @@ func (gr GitRepo) extractRepoDetailsFromSourceURL() (owner, repo, branch, versio
 
 func fileInterceptor(br *bufio.Writer) walker.FileInterceptor {
 	return func(file walker.File) error {
-		ext := filepath.Ext(file.Name)
-		if ext == ".yaml" || ext == ".yml" {
-			br.WriteString("\n---\n")
-			br.WriteString(file.Content)
-		}
+		tempPath := filepath.Join(os.TempDir(), utils.GetRandomAlphabetsOfDigit(5))
+		ProcessContent(br, tempPath, file.Path)
 		return nil
 	}
 }
