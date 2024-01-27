44f337d9db5b7ad3d3eef3176a9a31c24d77ea27
Pranav Singh <pranavsingh02@hotmail.com>
2024-01-27 14:53:58 +0530

    add utils for registry publish
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

---
+++ utils/google.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"context"
+	"encoding/base64"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+)
+
+
+func NewSheetSRV(cred string) (*sheets.Service, error) {
+	ctx := context.Background()
+	byt, _ := base64.StdEncoding.DecodeString(cred)
+	// authenticate and get configuration
+	config, err := google.JWTConfigFromJSON(byt, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		return nil, err
+	}
+	// create client with config and context
+	client := config.Client(ctx)
+	// create new service using client
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		return nil, err
+	}
+	return srv, nil
+}
