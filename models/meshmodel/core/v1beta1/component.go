7a1c604dce42a405b7c7c9f04253dc809ac3613a
MUzairS15 <muzair.shaikh810@gmail.com>
2024-03-21 17:13:47 +0530

    complete v1beta1 for component
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- models/meshmodel/core/v1beta1/component.go
+++ models/meshmodel/core/v1beta1/component.go
@@ -2,7 +2,6 @@ package v1beta1
 
 import (
 	"encoding/json"
-	"time"
 
 	"github.com/layer5io/meshkit/database"
 	types "github.com/layer5io/meshkit/models/meshmodel/entity"
@@ -23,32 +22,36 @@ const (
 	CUE  ComponentFormat = "CUE"
 )
 
+type entity struct {
+	TypeMeta
+	Type    string `json:"type,omitempty" yaml:"type"`
+	SubType string `json:"subType,omitempty" yaml:"subType"`
+	Schema  string `json:"schema,omitempty" yaml:"schema"`
+}
+
 // swagger:response ComponentDefinition
 // use NewComponent function for instantiating
 type ComponentDefinition struct {
-	ID uuid.UUID `json:"id,omitempty"`
-	TypeMeta
-	DisplayName     string                 `json:"displayName" gorm:"displayName"`
-	Format          ComponentFormat        `json:"format" yaml:"format"`
-	HostName        string                 `json:"hostname,omitempty"`
-	HostID          uuid.UUID              `json:"hostID,omitempty"`
-	DisplayHostName string                 `json:"displayhostname,omitempty"`
-	Metadata        map[string]interface{} `json:"metadata" yaml:"metadata"`
-	Model           Model                  `json:"model"`
-	Schema          string                 `json:"schema,omitempty" yaml:"schema"`
-	CreatedAt       time.Time              `json:"-"`
-	UpdatedAt       time.Time              `json:"-"`
+	ID                uuid.UUID              `json:"id,omitempty"`
+	SchemaVersion     string                 `json:"schemaVersion,omitempty" yaml:"schemaVersion"`
+	DefinitionVersion string                 `json:"definitionVersion,omitempty" yaml:"definitionVersion"`
+	DisplayName       string                 `json:"displayName" gorm:"displayName"`
+	Format            ComponentFormat        `json:"format" yaml:"format"`
+	Model             Model                  `json:"model"`
+	Metadata          map[string]interface{} `json:"metadata" yaml:"metadata"`
+	// Rename this here and in schema as well, it creates confustion b/w entities like comps. relationship. model, here entity means Pod/Deployment....
+	Entity entity `json:"entity,omitempty" yaml:"entity"`
 }
+
 type ComponentDefinitionDB struct {
-	ID      uuid.UUID `json:"id"`
-	ModelID uuid.UUID `json:"-" gorm:"index:idx_component_definition_dbs_model_id,column:modelID"`
-	TypeMeta
-	DisplayName string          `json:"displayName" gorm:"displayName"`
-	Format      ComponentFormat `json:"format" yaml:"format"`
-	Metadata    []byte          `json:"metadata" yaml:"metadata"`
-	Schema      string          `json:"schema,omitempty" yaml:"schema"`
-	CreatedAt   time.Time       `json:"-"`
-	UpdatedAt   time.Time       `json:"-"`
+	ID                uuid.UUID       `json:"id"`
+	SchemaVersion     string          `json:"schemaVersion,omitempty" yaml:"schemaVersion"`
+	DefinitionVersion string          `json:"definitionVersion,omitempty" yaml:"definitionVersion"`
+	DisplayName       string          `json:"displayName" gorm:"displayName"`
+	Format            ComponentFormat `json:"format" yaml:"format"`
+	ModelID           uuid.UUID       `json:"-" gorm:"index:idx_component_definition_dbs_model_id,column:modelID"`
+	Metadata          []byte          `json:"metadata" yaml:"metadata"`
+	Entity entity `json:"entity,omitempty" yaml:"entity" gorm:"entity"`
 }
 
 func (c ComponentDefinition) Type() types.EntityType {
@@ -66,7 +69,7 @@ func (c *ComponentDefinition) Create(db *database.Handler) (uuid.UUID, error) {
 		return uuid.UUID{}, err
 	}
 
-	if !utils.IsSchemaEmpty(c.Schema) {
+	if !utils.IsSchemaEmpty(c.Entity.Schema) {
 		c.Metadata["hasInvalidSchema"] = true
 	}
 	cdb := c.GetComponentDefinitionDB()
@@ -77,24 +80,24 @@ func (c *ComponentDefinition) Create(db *database.Handler) (uuid.UUID, error) {
 
 func (c *ComponentDefinition) GetComponentDefinitionDB() (cmd ComponentDefinitionDB) {
 	cmd.ID = c.ID
-	cmd.TypeMeta = c.TypeMeta
+	cmd.Entity.TypeMeta = c.Entity.TypeMeta
 	cmd.Format = c.Format
 	cmd.Metadata, _ = json.Marshal(c.Metadata)
 	cmd.DisplayName = c.DisplayName
-	cmd.Schema = c.Schema
+	cmd.Entity.Schema = c.Entity.Schema
 	return
 }
 
 func (cmd *ComponentDefinitionDB) GetComponentDefinition(model Model) (c ComponentDefinition) {
 	c.ID = cmd.ID
-	c.TypeMeta = cmd.TypeMeta
+	c.Entity.TypeMeta = cmd.Entity.TypeMeta
 	c.Format = cmd.Format
 	c.DisplayName = cmd.DisplayName
 	if c.Metadata == nil {
 		c.Metadata = make(map[string]interface{})
 	}
 	_ = json.Unmarshal(cmd.Metadata, &c.Metadata)
-	c.Schema = cmd.Schema
+	c.Entity.Schema = cmd.Entity.Schema
 	c.Model = model
 	return
 }
