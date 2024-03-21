6658ea223ea2e7e0bf2620a85c660a60fbd94a80
MUzairS15 <muzair.shaikh810@gmail.com>
2024-03-21 17:00:25 +0530

    add registrant/host as first class attribute for models
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

---
+++ models/meshmodel/registry/hostv1beta1.go
@@ -0,0 +1,121 @@
+package registry
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/layer5io/meshkit/database"
+	"github.com/layer5io/meshkit/models/oam/core/v1alpha1"
+	"github.com/layer5io/meshkit/utils/kubernetes"
+	"gorm.io/gorm"
+)
+
+var hostv1beta1CreationLock sync.Mutex //Each entity will perform a check and if the host already doesn't exist, it will create a host. This lock will make sure that there are no race conditions.
+
+type Hostv1beta1 struct {
+	ID        uuid.UUID `json:"-"`
+	Hostname  string
+	Port      int
+	Metadata  string
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+	IHost     IHost     `gorm:"-"`
+}
+
+func(h *Hostv1beta1) Create(db *database.Handler) (uuid.UUID, error) {
+	byt, err := json.Marshal(h)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	hID := uuid.NewSHA1(uuid.UUID{}, byt)
+	var host Host
+	hostv1beta1CreationLock.Lock()
+	defer hostv1beta1CreationLock.Unlock()
+	err = db.First(&host, "id = ?", hID).Error // check if the host already exists
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return uuid.UUID{}, err
+	}
+
+	// if not exists then create a new host and return the id
+	if err == gorm.ErrRecordNotFound {
+		h.ID = hID
+		err = db.Create(&h).Error
+		if err != nil {
+			return uuid.UUID{}, err
+		}
+		return h.ID, nil
+	}
+
+	// else return the id of the existing host
+	return host.ID, nil
+}
+
+func (h *Hostv1beta1) AfterFind(tx *gorm.DB) error {
+	switch h.Hostname {
+	case "artifacthub":
+		h.IHost = ArtifactHub{}
+	case "kubernetes":
+		h.IHost = Kubernetes{}
+	default: // do nothing if the host is not pre-unknown. Currently adapters fall into this case.
+		return nil
+	}
+	return nil
+}
+
+// Each host from where meshmodels can be generated needs to implement this interface
+// HandleDependents, contains host specific logic for provisioning required CRDs/operators for corresponding components.
+type _IHost interface {
+	HandleDependents(comp v1alpha1.Component, kc *kubernetes.Client, isDeploy bool) (string, error)
+	String() string
+}
+
+type _ArtifactHub struct{}
+
+func (ah _ArtifactHub) HandleDependents(comp v1alpha1.Component, kc *kubernetes.Client, isDeploy bool) (summary string, err error) {
+	source_uri := comp.Annotations[fmt.Sprintf("%s.model.source_uri", v1alpha1.MesheryAnnotationPrefix)]
+	act := kubernetes.UNINSTALL
+	if isDeploy {
+		act = kubernetes.INSTALL
+	}
+
+	if source_uri != "" {
+		err = kc.ApplyHelmChart(kubernetes.ApplyHelmChartConfig{
+			URL:                    source_uri,
+			Namespace:              comp.Namespace,
+			CreateNamespace:        true,
+			Action:                 act,
+			SkipUpgradeIfInstalled: true,
+		})
+		if err != nil {
+			if !isDeploy {
+				summary = fmt.Sprintf("error undeploying dependent helm chart for %s, please proceed with manual uninstall or try again", comp.Name)
+			} else {
+				summary = fmt.Sprintf("error deploying dependent helm chart for %s, please procced with manual install or try again", comp.Name)
+			}
+		} else {
+			if !isDeploy {
+				summary = fmt.Sprintf("Undeployed dependent helm chart for %s", comp.Name)
+			} else {
+				summary = fmt.Sprintf("Deployed dependent helm chart for %s", comp.Name)
+			}
+		}
+	}
+	return summary, err
+}
+
+func (ah _ArtifactHub) String() string {
+	return "artifacthub"
+}
+
+type _Kubernetes struct{}
+
+func (k _Kubernetes) HandleDependents(comp v1alpha1.Component, kc *kubernetes.Client, isDeploy bool) (summary string, err error) {
+	return summary, err
+}
+
+func (k _Kubernetes) String() string {
+	return "kubernetes"
+}
