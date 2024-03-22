35d7e3eb398b18ec3691e783ea5c2dc952e384da
MUzairS15 <muzair.shaikh810@gmail.com>
2024-03-22 17:04:15 +0530

    update as per latest changes to schema
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- models/meshmodel/core/v1beta1/relationship.go
+++ models/meshmodel/core/v1beta1/relationship.go
@@ -2,7 +2,6 @@ package v1beta1
 
 import (
 	"encoding/json"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/layer5io/meshkit/database"
@@ -12,35 +11,33 @@ import (
 // Remove additional DB structs from all entites they are used just for marshaling & unmarshlling and tarcking modelID vs model(modelID when referenced in DB and full model when sending data, use foregin key references tag of gorm for that as used in MeshSync tables) when saving retrivng from database, insted use gorm' serailization tag as used in events.
 type RelationshipDefinition struct {
 	ID uuid.UUID `json:"id"`
-	TypeMeta
-	Model           Model                  `json:"model"`
-	HostName        string                 `json:"hostname"`
-	HostID          uuid.UUID              `json:"hostID"`
-	DisplayHostName string                 `json:"displayhostname"`
-	Metadata        map[string]interface{} `json:"metadata" yaml:"metadata"`
+	VersionMeta
+	Kind             string                 `json:"kind,omitempty" yaml:"kind"`
+	RelationshipType string                 `json:"type" yaml:"type" gorm:"type"`
+	SubType          string                 `json:"subType" yaml:"subType" gorm:"subType"`
+	Model            Model                  `json:"model"`
+	HostName         string                 `json:"hostname"`
+	HostID           uuid.UUID              `json:"hostID"`
+	DisplayHostName  string                 `json:"displayhostname"`
+	Metadata         map[string]interface{} `json:"metadata" yaml:"metadata"`
 	// The property has been named RelationshipType instead of Type to avoid collision from Type() function, which enables support for dynamic type.
 	// Though, the column name and the json representation is "type".
-	RelationshipType string                   `json:"type" yaml:"type" gorm:"type"`
-	SubType          string                   `json:"subType" yaml:"subType" gorm:"subType"`
-	EvaluationQuery  string                   `json:"evaluationQuery" yaml:"evaluationQuery" gorm:"evaluationQuery"`
-	Selectors        []map[string]interface{} `json:"selectors" yaml:"selectors"`
-	CreatedAt        time.Time                `json:"-"`
-	UpdatedAt        time.Time                `json:"-"`
+	EvaluationQuery string                   `json:"evaluationQuery" yaml:"evaluationQuery" gorm:"evaluationQuery"`
+	Selectors       []map[string]interface{} `json:"selectors" yaml:"selectors"`
 }
 
 type RelationshipDefinitionDB struct {
 	ID      uuid.UUID `json:"id"`
 	ModelID uuid.UUID `json:"-" gorm:"index:idx_relationship_definition_dbs_model_id,column:modelID"`
-	TypeMeta
-	Metadata []byte `json:"metadata" yaml:"metadata"`
+	VersionMeta
+	Kind             string `json:"kind,omitempty" yaml:"kind"`
+	RelationshipType string `json:"type" yaml:"type" gorm:"type"`
+	SubType          string `json:"subType" yaml:"subType"`
+	Metadata         []byte `json:"metadata" yaml:"metadata"`
 	// The property has been named RelationshipType instead of Type to avoid collision from Type() function, which enables support for dynamic type.
 	// Though, the column name and the json representation is "type".
-	RelationshipType string    `json:"type" yaml:"type" gorm:"type"`
-	SubType          string    `json:"subType" yaml:"subType"`
-	EvaluationQuery  string    `json:"evaluationQuery" yaml:"evaluationQuery" gorm:"evaluationQuery"`
-	Selectors        []byte    `json:"selectors" yaml:"selectors"`
-	CreatedAt        time.Time `json:"-"`
-	UpdatedAt        time.Time `json:"-"`
+	EvaluationQuery string    `json:"evaluationQuery" yaml:"evaluationQuery" gorm:"evaluationQuery"`
+	Selectors       []byte    `json:"selectors" yaml:"selectors"`
 }
 
 // For now, only filtering by Kind and SubType are allowed.
@@ -83,7 +80,7 @@ func (r *RelationshipDefinition) Create(db *database.Handler) (uuid.UUID, error)
 
 func (r *RelationshipDefinition) GetRelationshipDefinitionDB() (rdb RelationshipDefinitionDB) {
 	rdb.ID = r.ID
-	rdb.TypeMeta = r.TypeMeta
+	rdb.VersionMeta = r.VersionMeta
 	rdb.Metadata, _ = json.Marshal(r.Metadata)
 	rdb.Selectors, _ = json.Marshal(r.Selectors)
 	rdb.Kind = r.Kind
@@ -96,7 +93,7 @@ func (r *RelationshipDefinition) GetRelationshipDefinitionDB() (rdb Relationship
 
 func (rdb *RelationshipDefinitionDB) GetRelationshipDefinition(m Model) (r RelationshipDefinition) {
 	r.ID = rdb.ID
-	r.TypeMeta = rdb.TypeMeta
+	r.VersionMeta = rdb.VersionMeta
 	if r.Metadata == nil {
 		r.Metadata = make(map[string]interface{})
 	}
