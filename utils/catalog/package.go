76348fee0060be7978560aecbaac7d7b6a1c62ec
MUzairS15 <muzair.shaikh810@gmail.com>
2024-06-12 17:16:17 +0530

    artifacthub pkg generation
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

---
+++ utils/catalog/package.go
@@ -0,0 +1,55 @@
+package catalog
+
+import (
+	"fmt"
+
+	"github.com/layer5io/meshkit/models/catalog/v1beta1"
+)
+
+func BuildArtifactHubPkg(name, downloadURL, user, version, createdAt string, catalogData *v1beta1.CatalogData) *ArtifactHubMetadata {
+	artifacthubPkg := &ArtifactHubMetadata{
+		Name:        name,
+		Description: catalogData.PatternInfo,
+		Provider: Provider{
+			Name: user,
+		},
+		Links: []Link{
+			{
+				Name: "download",
+				URL:  downloadURL, // this depends on where the design is stored by the user, we can give remote provider URL otherwise
+			},
+			{
+				Name: "Meshery Catalog",
+				URL:  "https://meshery.io/catalog",
+			},
+		},
+		HomeURL:   "https://docs.meshery.io/concepts/logical/designs",
+		Version:   version,
+		CreatedAt: createdAt,
+		License:   "Apache-2.0",
+		LogoURL:   "https://raw.githubusercontent.com/meshery/meshery.io/0b8585231c6e2b3251d38f749259360491c9ee6b/assets/images/brand/meshery-logo.svg",
+		Install:   "mesheryctl design import -f",
+		Readme:    fmt.Sprintf("%s \n ##h4 Caveats and Consideration \n", catalogData.PatternCaveats),
+	}
+
+	if len(catalogData.SnapshotURL) > 0 {
+		artifacthubPkg.Screenshots = append(artifacthubPkg.Screenshots, Screenshot{
+			Title: "MeshMap Snapshot",
+			URL:   catalogData.SnapshotURL[0],
+		})
+
+		if len(catalogData.SnapshotURL) > 1 {
+			artifacthubPkg.Screenshots = append(artifacthubPkg.Screenshots, Screenshot{
+				Title: "MeshMap Snapshot",
+				URL:   catalogData.SnapshotURL[1],
+			})
+		}
+	}
+
+	artifacthubPkg.Screenshots = append(artifacthubPkg.Screenshots, Screenshot{
+		Title: "Meshery Project",
+		URL:   "https://raw.githubusercontent.com/meshery/meshery.io/master/assets/images/logos/meshery-gradient.png",
+	})
+
+	return artifacthubPkg
+}
