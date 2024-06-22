e183bc33988050d65f234cd14eff4fcecdc0490c
MUzairS15 <muzair.shaikh810@gmail.com>
2024-06-22 21:02:14 +0530

    add probable cause and suggested remediation
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- models/patterns/error.go
+++ models/patterns/error.go
@@ -2,11 +2,10 @@ package patterns
 
 import "github.com/layer5io/meshkit/errors"
 
-
 const (
 	ErrInvalidVersionCode = ""
 )
 
 func ErrInvalidVersion(err error) error {
-	return errors.New(ErrInvalidVersionCode, errors.Alert, []string{}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrInvalidVersionCode, errors.Alert, []string{"invalid/incompatible semver version"}, []string{err.Error()}, []string{"version history for the content has been tampered outside meshery"}, []string{"rolllback to one of the previous version"})
 }
