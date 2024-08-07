091910c2c7e714d87005aa6f68d7f998dffc09ac
MUzairS15 <muzair.shaikh810@gmail.com>
2024-08-07 19:01:30 +0530

    run gofmt
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- models/registration/register.go
+++ models/registration/register.go
@@ -9,28 +9,28 @@ import (
 
 // packaingUnit is the representation of the atomic unit that can be registered into the capabilities registry
 type packagingUnit struct {
-	model v1beta1.Model
-	components []v1beta1.ComponentDefinition
+	model         v1beta1.Model
+	components    []v1beta1.ComponentDefinition
 	relationships []v1alpha2.RelationshipDefinition
-	_ []v1beta1.PolicyDefinition
+	_             []v1beta1.PolicyDefinition
 }
 
 type RegistrationHelper struct {
-	regManager       *meshmodel.RegistryManager
-    regErrStore      RegistrationErrorStore
+	regManager  *meshmodel.RegistryManager
+	regErrStore RegistrationErrorStore
 }
 
 func NewRegistrationHelper(regm *meshmodel.RegistryManager, regErrStore RegistrationErrorStore) RegistrationHelper {
-	return RegistrationHelper{ regManager: regm, regErrStore: regErrStore}
+	return RegistrationHelper{regManager: regm, regErrStore: regErrStore}
 }
 
 /*
-	Register will accept a RegisterableEntity (dir, tar or oci for now).
+Register will accept a RegisterableEntity (dir, tar or oci for now).
 */
 func (rh *RegistrationHelper) Register(entity RegisterableEntity) error {
 	// get the packaging units
 	pu, err := entity.PkgUnit(rh.regErrStore)
-	if(err != nil){
+	if err != nil {
 		// given input is not a valid model, or could not walk the directory
 		return err
 	}
@@ -38,30 +38,29 @@ func (rh *RegistrationHelper) Register(entity RegisterableEntity) error {
 	return rh.register(pu)
 }
 
-
 /*
-	register will return an error if it is not able to register the `model`.
-	If there are errors when registering other entities, they are handled properly but does not stop the registration process.
+register will return an error if it is not able to register the `model`.
+If there are errors when registering other entities, they are handled properly but does not stop the registration process.
 */
-func (rh *RegistrationHelper)register(pkg packagingUnit) error {
+func (rh *RegistrationHelper) register(pkg packagingUnit) error {
 	// 1. Register the model
 	model := pkg.model
 
 	// Dont register anything else if registrant is not there
-	if(model.Registrant.Hostname == ""){
+	if model.Registrant.Hostname == "" {
 		err := ErrMissingRegistrant(model.Name)
-		rh.regErrStore.InsertEntityRegError(model.Registrant.Hostname, "",entity.Model, model.Name, err)
+		rh.regErrStore.InsertEntityRegError(model.Registrant.Hostname, "", entity.Model, model.Name, err)
 		return err
 	}
 	_, _, err := rh.regManager.RegisterEntity(
-		v1beta1.Host{Hostname: model.Registrant.Hostname,},
+		v1beta1.Host{Hostname: model.Registrant.Hostname},
 		&model,
-		)
+	)
 
 	// If model cannot be registered, don't register anything else
 	if err != nil {
 		err = ErrRegisterEntity(err, string(model.Type()), model.DisplayName)
-		rh.regErrStore.InsertEntityRegError(model.Registrant.Hostname, "",entity.Model, model.Name, err)
+		rh.regErrStore.InsertEntityRegError(model.Registrant.Hostname, "", entity.Model, model.Name, err)
 		return err
 	}
 
@@ -71,13 +70,13 @@ func (rh *RegistrationHelper)register(pkg packagingUnit) error {
 	for _, comp := range pkg.components {
 		comp.Model = model
 		_, _, err := rh.regManager.RegisterEntity(
-		v1beta1.Host{Hostname: hostname,},
-		&comp,
+			v1beta1.Host{Hostname: hostname},
+			&comp,
 		)
-	if err != nil {
-		err = ErrRegisterEntity(err, string(comp.Type()), comp.DisplayName)
-		rh.regErrStore.InsertEntityRegError(hostname, modelName ,entity.ComponentDefinition, comp.DisplayName, err)
-	}
+		if err != nil {
+			err = ErrRegisterEntity(err, string(comp.Type()), comp.DisplayName)
+			rh.regErrStore.InsertEntityRegError(hostname, modelName, entity.ComponentDefinition, comp.DisplayName, err)
+		}
 	}
 
 	// 3. Register relationships
@@ -88,8 +87,8 @@ func (rh *RegistrationHelper)register(pkg packagingUnit) error {
 		}, &rel)
 		if err != nil {
 			err = ErrRegisterEntity(err, string(rel.Type()), rel.Kind)
-			rh.regErrStore.InsertEntityRegError(hostname, modelName ,entity.RelationshipDefinition, rel.ID.String(), err)
+			rh.regErrStore.InsertEntityRegError(hostname, modelName, entity.RelationshipDefinition, rel.ID.String(), err)
 		}
 	}
-    return nil
+	return nil
 }
