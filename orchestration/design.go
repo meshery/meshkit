6646c25142569a839937c94d401889324aa12082
aabidsofi19 <mailtoaabid01@gmail.com>
2024-12-19 16:48:42 +0530

    add constants for meshery specific labels
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>
    Signed-off-by: weilirs <weilirs@umich.edu>

---
+++ orchestration/design.go
@@ -0,0 +1,7 @@
+package orchestration
+
+const (
+	ResourceSourceDesignIdLabelKey    = "design.meshery.io/id"
+	ResourceSourceDesignNameLabelKey  = "design.meshery.io/name"
+	ResourceSourceComponentIdLabelKey = "component.meshery.io/id"
+)
