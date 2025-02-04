008b46835db7ab74eeebd3430fc8c48b4f999546
Riya Garg <riyag1452003@gmail.com>
2025-02-04 14:21:57 +0530

    revert svg newline removal change
    Signed-off-by: Riya Garg <riyag1452003@gmail.com>

--- utils/csv/csv.go
+++ utils/csv/csv.go
@@ -87,11 +87,6 @@ func (c *CSV[E]) Parse(ch chan E, errorChan chan error) error {
 						}
 					}
 
-					// To keep uniform formatting. This avoids formatting changes to be pushed in commits
-					if attribute == "svgwhite" || attribute == "svgcolor" || attribute == "svgcomplete" {
-						value = strings.ReplaceAll(value, "\n", "")
-					}
-					
 					data[attribute] = value
 				}
 			}
