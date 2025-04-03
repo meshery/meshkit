499792ff81643de743cbb4d1da7ca7370b47362a
Riya Garg <riyag1452003@gmail.com>
2025-04-03 21:01:01 +0530

    improve error logs for invalid github source url while generating a model
    Signed-off-by: Riya Garg <riyag1452003@gmail.com>

--- generators/github/error.go
+++ generators/github/error.go
@@ -12,9 +12,9 @@ const (
 )
 
 func ErrGenerateGitHubPackage(err error, pkgName string) error {
-	return errors.New(ErrGenerateGitHubPackageCode, errors.Alert, []string{fmt.Sprintf("error generate package for %s", pkgName)}, []string{err.Error()}, []string{"invalid sourceurl provided", "repository might be private"}, []string{"provided sourceURL according to the format", "provide approparite credentials to clone a private repository"})
+	return errors.New(ErrGenerateGitHubPackageCode, errors.Alert, []string{fmt.Sprintf("Error generating package for %s", pkgName)}, []string{fmt.Sprintf("Error generating package for %s. %s", pkgName, err.Error())}, []string{"Invalid source URL provided", "Repository might be private"}, []string{"Ensure source URL follows the format: git://[owner]/[repositoryname]/[branch]/[path from the root of repository]", "Make sure the path to CRDs is correct in the URL", "Private repositories aren't supported currently, ensure that provided URL is from a public repository"})
 }
 
 func ErrInvalidGitHubSourceURL(err error) error {
-	return errors.New(ErrInvalidGitHubSourceURLCode, errors.Alert, []string{}, []string{err.Error()}, []string{"sourceURL provided might be invalid", "provided repo/version tag does not exist"}, []string{"ensure source url follows the format: git://<owner>/<repositoryname>/<branch>/<version>/<path from the root of repository>"})
+	return errors.New(ErrInvalidGitHubSourceURLCode, errors.Alert, []string{"Error parsing the source URL"}, []string{fmt.Sprintf("Error parsing the source URL: %s", err.Error())}, []string{"Source URL provided might be invalid", "Provided repo/version tag does not exist"}, []string{"Ensure source url follows the format: git://[owner]/[repositoryname]/[branch]/[path from the root of repository]"})
 }
