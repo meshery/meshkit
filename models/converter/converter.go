45a617e8da95c7351e939840aeb59a88f9faa004
fyzanshaik <fyzan.shaik@gmail.com>
2025-04-05 03:18:05 +0530

    Modify converter to create helm-chart converter
    Signed-off-by: fyzanshaik <fyzan.shaik@gmail.com>

--- models/converter/converter.go
+++ models/converter/converter.go
@@ -12,6 +12,9 @@ func NewFormatConverter(format DesignFormat) (ConvertFormat, error) {
 	switch format {
 	case K8sManifest:
 		return &converter.K8sConverter{}, nil
+	case HelmChart:
+		// fmt.Println("HelmChart format detected, creating HelmConverter")
+		return &converter.HelmConverter{}, nil
 	default:
 		return nil, ErrUnknownFormat(format)
 	}
