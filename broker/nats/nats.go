442318a45e102340928179577cc085514fb16d23
Mohit Nagaraj <mohitnagaraj20@gmail.com>
2025-06-07 19:13:59 +0000

    fix: nats: update package
    Signed-off-by: Mohit Nagaraj <mohitnagaraj20@gmail.com>

--- broker/nats/nats.go
+++ broker/nats/nats.go
@@ -1,6 +1,7 @@
 package nats
 
 import (
+	"encoding/json"
 	"log"
 	"strings"
 	"sync"
@@ -25,8 +26,8 @@ type Options struct {
 
 // Nats will implement Nats subscribe and publish functionality
 type Nats struct {
-	ec *nats.EncodedConn
-	wg *sync.WaitGroup
+  conn *nats.Conn
+  wg   *sync.WaitGroup
 }
 
 // New - constructor
@@ -57,55 +58,66 @@ func New(opts Options) (broker.Handler, error) {
 		return nil, ErrConnect(err)
 	}
 
-	ec, err := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	if err != nil {
-		return nil, ErrEncodedConn(err)
-	}
-
-	return &Nats{ec: ec}, nil
+	return &Nats{conn: nc, wg: &sync.WaitGroup{}}, nil
 }
+
 func (n *Nats) ConnectedEndpoints() (endpoints []string) {
-	for _, server := range n.ec.Conn.Servers() {
+	for _, server := range n.conn.Servers() {
 		endpoints = append(endpoints, strings.TrimPrefix(server, "nats://"))
 	}
 	return
 }
 
 func (n *Nats) Info() string {
-	if n.ec == nil || n.ec.Conn == nil {
+	if n.conn == nil {
 		return broker.NotConnected
 	}
-	return n.ec.Conn.Opts.Name
+	return n.conn.Opts.Name
 }
 
 func (n *Nats) CloseConnection() {
-	n.ec.Close()
+    if n.conn != nil {
+        if err := n.conn.Drain(); err != nil {
+            log.Printf("nats: drain error: %v", err)
+        }
+        n.conn.Close()
+    }
 }
+
 
 // Publish - to publish messages
 func (n *Nats) Publish(subject string, message *broker.Message) error {
-	err := n.ec.Publish(subject, message)
+	b, err := json.Marshal(message)
 	if err != nil {
 		return ErrPublish(err)
 	}
-	return nil
+	return n.conn.Publish(subject, b)
 }
 
 // PublishWithChannel - to publish messages with channel
 func (n *Nats) PublishWithChannel(subject string, msgch chan *broker.Message) error {
-	err := n.ec.BindSendChan(subject, msgch)
-	if err != nil {
-		return ErrPublish(err)
-	}
-	return nil
+    go func() {
+        for msg := range msgch {
+            b, err := json.Marshal(msg)
+            if err != nil {
+                log.Printf("nats: JSON marshal error: %v", err)
+                continue
+            }
+            if err := n.conn.Publish(subject, b); err != nil {
+                log.Printf("nats: publish error for subject %s: %v", subject, err)
+            }
+        }
+    }()
+    return nil
 }
+
 
 // Subscribe - for subscribing messages
 // TODO Ques: Do we want to unsubscribe
 // TODO will the method-user just subsribe, how will it handle the received messages?
 func (n *Nats) Subscribe(subject, queue string, message []byte) error {
 	n.wg.Add(1)
-	_, err := n.ec.QueueSubscribe(subject, queue, func(msg *nats.Msg) {
+	_, err := n.conn.QueueSubscribe(subject, queue, func(msg *nats.Msg) {
 		message = msg.Data
 		n.wg.Done()
 	})
@@ -119,7 +131,12 @@ func (n *Nats) Subscribe(subject, queue string, message []byte) error {
 
 // SubscribeWithChannel will publish all the messages received to the given channel
 func (n *Nats) SubscribeWithChannel(subject, queue string, msgch chan *broker.Message) error {
-	_, err := n.ec.BindRecvQueueChan(subject, queue, msgch)
+	_, err := n.conn.QueueSubscribe(subject, queue, func(m *nats.Msg) {
+    	var msg broker.Message
+    	if err := json.Unmarshal(m.Data, &msg); err == nil {
+      		msgch <- &msg
+    	}
+  	})
 	if err != nil {
 		return ErrQueueSubscribe(err)
 	}
@@ -153,8 +170,5 @@ func (in *Nats) DeepCopyObject() broker.Handler {
 // Check if the connection object is empty
 func (in *Nats) IsEmpty() bool {
 	empty := &Nats{}
-	if in == nil || *in == *empty {
-		return true
-	}
-	return false
+	return in == nil || *in == *empty
 }
