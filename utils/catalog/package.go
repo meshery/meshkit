42687a23916dc923142ec63bf176a7ad66b5473c
ShashaankS <shashaank.srivastava04@gmail.com>
2025-07-11 03:52:08 +0530

    fix: handle time.TIme & nil createdAt time in ArtifactHub package builder
    Signed-off-by: ShashaankS <shashaank.srivastava04@gmail.com>

--- utils/catalog/package.go
+++ utils/catalog/package.go
@@ -2,12 +2,22 @@ package catalog
 
 import (
 	"fmt"
-	"github.com/meshery/meshkit/models/catalog/v1alpha1"
 	"regexp"
 	"strings"
+	"time"
+
+	"github.com/meshery/meshkit/models/catalog/v1alpha1"
 )
 
-func BuildArtifactHubPkg(name, downloadURL, user, version, createdAt string, catalogData *v1alpha1.CatalogData) *ArtifactHubMetadata {
+func BuildArtifactHubPkg(name, downloadURL, user, version string, createdAt *time.Time, catalogData *v1alpha1.CatalogData) *ArtifactHubMetadata {
+	
+	var createdAtStr string
+    if createdAt != nil {
+        createdAtStr = createdAt.Format(time.RFC3339)
+    } else {
+        createdAtStr = time.Now().Format(time.RFC3339)
+    }
+
 	artifacthubPkg := &ArtifactHubMetadata{
 		Name:        toKebabCase(name),
 		DisplayName: name,
@@ -27,7 +37,7 @@ func BuildArtifactHubPkg(name, downloadURL, user, version, createdAt string, cat
 		},
 		HomeURL:   "https://docs.meshery.io/concepts/logical/designs",
 		Version:   valueOrElse(version, "0.0.1"),
-		CreatedAt: createdAt,
+		CreatedAt: createdAtStr,
 		License:   "Apache-2.0",
 		LogoURL:   "https://raw.githubusercontent.com/meshery/meshery.io/0b8585231c6e2b3251d38f749259360491c9ee6b/assets/images/brand/meshery-logo.svg",
 		Install:   "mesheryctl design import -f",
