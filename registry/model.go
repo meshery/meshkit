fe4c17016d22e3cd928935f3e7b0a674d25c1345
Malka123456 <malka988276@gmail.com>
2025-07-10 16:39:00 +0530

    fix: make model name comparison case-insensitive using EqualFold
    Signed-off-by: Malka123456 <malka988276@gmail.com>

--- registry/model.go
+++ registry/model.go
@@ -839,7 +839,7 @@ func InvokeGenerationFromSheet(wg *sync.WaitGroup, path string, modelsheetID, co
 	// Iterate models from the spreadsheet
 	for _, model := range modelCSVHelper.Models {
 
-		if modelName != "" && modelName != model.Model {
+		if modelName != "" && ! strings.EqualFold(modelName, model.Model) {
 			continue
 		}
 		totalAvailableModels++
