a2db991170a4689925c2aa6b90a65d1c049bfc36
HeerakKashyap <kashyapheerk@email.com>
2025-07-27 23:19:11 +0530

    fix: resolve linting issues in registration logic

--- models/registration/utils.go
+++ models/registration/utils.go
@@ -21,28 +21,28 @@ func getEntity(byt []byte) (et entity.Entity, _ error) {
 	var sv schemaVersion
 	err := encoding.Unmarshal(byt, &sv)
 	if err != nil || sv.SchemaVersion == "" {
-		return nil, ErrGetEntity(fmt.Errorf("Does not contain versionmeta"))
+		return nil, ErrGetEntity(fmt.Errorf("does not contain versionmeta"))
 	}
 	switch sv.SchemaVersion {
 	case schemav1beta1.ComponentSchemaVersion:
 		var compDef component.ComponentDefinition
 		err := encoding.Unmarshal(byt, &compDef)
 		if err != nil {
-			return nil, ErrGetEntity(fmt.Errorf("Invalid component definition: %s", err.Error()))
+			return nil, ErrGetEntity(fmt.Errorf("invalid component definition: %s", err.Error()))
 		}
 		et = &compDef
 	case schemav1beta1.ModelSchemaVersion:
 		var model model.ModelDefinition
 		err := encoding.Unmarshal(byt, &model)
 		if err != nil {
-			return nil, ErrGetEntity(fmt.Errorf("Invalid model definition: %s", err.Error()))
+			return nil, ErrGetEntity(fmt.Errorf("invalid model definition: %s", err.Error()))
 		}
 		et = &model
 	case v1alpha3.RelationshipSchemaVersion:
 		var rel relationship.RelationshipDefinition
 		err := encoding.Unmarshal(byt, &rel)
 		if err != nil {
-			return nil, ErrGetEntity(fmt.Errorf("Invalid relationship definition: %s", err.Error()))
+			return nil, ErrGetEntity(fmt.Errorf("invalid relationship definition: %s", err.Error()))
 		}
 		et = &rel
 	case "connections.meshery.io/v1beta1":
