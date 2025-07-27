8c9564e7b208c093fbf0c63bf1c9c500e172c567
HeerakKashyap <kashyapheerk@email.com>
2025-07-27 06:17:36 +0530

    feat: add ConnectionDefinition entity type and core structure
    - Add ConnectionDefinition to EntityType enum
    - Create ConnectionDefinition struct with all required methods
    - Implement entity.Entity interface for ConnectionDefinition
    - Use connection entity from schemas repository as requested
    
    This is part 1 of splitting the connection definition support into smaller PRs.
    
    Signed-off-by: HeerakKashyap <kashyapheerk@email.com>

---
+++ models/meshmodel/core/v1beta1/connection.go
@@ -0,0 +1,66 @@
+package v1beta1
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofrs/uuid"
+	"github.com/meshery/meshkit/database"
+	"github.com/meshery/meshkit/models/meshmodel/entity"
+	"github.com/meshery/schemas/models/v1beta1/connection"
+	v1beta1 "github.com/meshery/schemas/models/v1beta1/model"
+	"gorm.io/gorm/clause"
+)
+
+// ConnectionDefinition represents a connection definition entity
+type ConnectionDefinition struct {
+	ID         uuid.UUID               `json:"-" gorm:"primarykey"`
+	Kind       string                  `json:"kind,omitempty" yaml:"kind"`
+	Version    string                  `json:"version,omitempty" yaml:"version"`
+	ModelID    uuid.UUID               `json:"-" gorm:"column:modelID"`
+	Model      v1beta1.ModelDefinition `json:"model"`
+	SubType    string                  `json:"subType" yaml:"subType"`
+	Connection connection.Connection   `json:"connection" yaml:"connection"`
+	CreatedAt  time.Time               `json:"-"`
+	UpdatedAt  time.Time               `json:"-"`
+}
+
+func (c ConnectionDefinition) GetID() uuid.UUID {
+	return c.ID
+}
+
+func (c *ConnectionDefinition) GenerateID() (uuid.UUID, error) {
+	return uuid.NewV4()
+}
+
+func (c ConnectionDefinition) Type() entity.EntityType {
+	return entity.ConnectionDefinition
+}
+
+func (c *ConnectionDefinition) GetEntityDetail() string {
+	return fmt.Sprintf("type: %s, definition version: %s, name: %s, model: %s, version: %s", c.Type(), c.Version, c.Kind, c.Model.Name, c.Model.Version)
+}
+
+func (c *ConnectionDefinition) Create(db *database.Handler, hostID uuid.UUID) (uuid.UUID, error) {
+	id, idErr := c.GenerateID()
+	if idErr != nil {
+		return uuid.UUID{}, idErr
+	}
+	c.ID = id
+
+	mid, err := c.Model.Create(db, hostID)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	c.ModelID = mid
+	err = db.Omit(clause.Associations).Create(&c).Error
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return c.ID, nil
+}
+
+func (c *ConnectionDefinition) UpdateStatus(db *database.Handler, status entity.EntityStatus) error {
+	// TODO: Implement status update logic for connection definitions
+	return nil
+}
