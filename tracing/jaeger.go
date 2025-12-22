7bb75f74fab8ca01fb21292d9d807f88f32b9756
Lee Calcote <lee.calcote@layer5.io>
2025-12-22 09:37:15 -0600

    Delete tracing/jaeger.go
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- tracing/jaeger.go
+++
@@ -1,6 +0,0 @@
-package tracing
-
-// This file is kept for backward compatibility but the implementation
-// has been moved to tracing.go with modern OpenTelemetry APIs.
-// The generic InitTracer function in tracing.go supports OTLP exporters
-// which are compatible with Jaeger (and other backends) via the OTLP protocol.
