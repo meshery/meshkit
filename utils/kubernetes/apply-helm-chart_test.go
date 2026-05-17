6116010de71955bbeda06de0a679dadcdbb16697
reaper8055 <11490705+reaper8055@users.noreply.github.com>
2026-05-17 19:53:42 +0530

    refactor: added setupKubeConfig method
    Signed-off-by: reaper8055 <11490705+reaper8055@users.noreply.github.com>

---
+++ utils/kubernetes/apply-helm-chart_test.go
@@ -0,0 +1,29 @@
+package kubernetes
+
+import (
+	"testing"
+	// "k8s.io/cli-runtime/pkg/genericclioptions"
+	// "k8s.io/client-go/rest"
+)
+
+func Test_setupKubeConfig(t *testing.T) {
+	// tests := []struct {
+	// 	name            string
+	// 	client          *Client
+	// 	wantKubeConfi   *genericclioptions.ConfigFlags
+	// 	wantCleanupFunc func()
+	// 	wantErr         error
+	// }{
+	// 	{
+	// 		name: "use exec auth in kubeconfig",
+	// 		client: &Client{
+	// 			RestConfig: rest.Config{
+	// 				ExecProvider: nil,
+	// 				BearerToken:  "",
+	// 			},
+	// 		},
+	// 		wantKubeConfi: *genericclioptions.ConfigFlags{
+	// 		},
+	// 	},
+	// }
+}
